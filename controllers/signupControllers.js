9eec22fac33e711fa0c8cef3255eea9650ff37c5
singhalex <singhkalex@gmail.com>
2024-05-15 16:41:48 -0700

    Rename signupController

--- controllers/signupControllers.js
+++ controllers/signupControllers.js
@@ -7,3 +7,14 @@ const { body, validationResult } = require("express-validator");
 exports.signup_get = asyncHandler(async (req, res, next) => {
   res.render("signup");
 });
+
+exports.signup_post = asyncHandler(async (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  const result = await user.save();
+
+  res.redirect("/");
+});
