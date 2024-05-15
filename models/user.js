7e0e5f6c1f812abeea2316f2bbb72f7a93196169
singhalex <singhkalex@gmail.com>
2024-05-15 16:43:49 -0700

    Change User schema

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 30 },
-  last_name: { type: String, required: true, maxLength: 30 },
+  username: { type: String, required: true, maxLength: 30 },
+  password: { type: String, required: true, maxLength: 30 },
 });
 
 UserSchema.virtual("full_name").get(function () {
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const signupController = require("../controllers/userControllers");
+const signupController = require("../controllers/signupControllers");
 
 // Get signup page
 router.get("/", signupController.signup_get);
+
+// Signup
+router.post("/", signupController.signup_post);
 
 module.exports = router;
