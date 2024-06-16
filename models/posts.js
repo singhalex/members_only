fa883b36902159a3df364c29eef10657e6d87692
singhalex <singhkalex@gmail.com>
2024-06-15 22:19:28 -0700

    Change body field to message

--- models/posts.js
+++ models/posts.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   title: { type: String, required: true, minLength: 3, maxLength: 50 },
-  body: { type: String, required: true, minLength: 3, maxLength: 200 },
+  message: { type: String, required: true, minLength: 3, maxLength: 200 },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
