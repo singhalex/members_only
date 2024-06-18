de7f2a3492134c44d4cce28dc08ec6f512a0e741
singhalex <singhkalex@gmail.com>
2024-06-17 19:27:44 -0700

    Retrieve and send posts to index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
+const Post = require("../models/posts");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", user: res.locals.currentUser });
+router.get("/", async function (req, res, next) {
+  const allPosts = await Post.find().populate("author").exec();
+
+  res.render("index", {
+    user: res.locals.currentUser,
+    posts: allPosts.reverse(),
+  });
 });
 
 module.exports = router;
