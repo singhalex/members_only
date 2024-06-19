847c0a1f7db67deb0cc42b18a03793998de8121a
singhalex <singhkalex@gmail.com>
2024-06-18 22:07:33 -0700

    Delete posts logic

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,6 @@
 const Post = require("../models/posts");
 const User = require("../models/user");
+const { DateTime } = require("luxon");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
@@ -8,14 +9,12 @@ exports.post = [
   // Validate and sanitize fields
   body("title")
     .trim()
-    .isLength({ min: 3 })
-    .escape()
+    .isLength({ min: 3, max: 50 })
     .withMessage("Title must be between 3 and 50 characters. "),
   body("message")
     .trim()
-    .isLength({ min: 3 })
-    .escape()
-    .withMessage("Message must be between 3 and 200 characters."),
+    .isLength({ min: 3, max: 400 })
+    .withMessage("Message must be between 3 and 400 characters."),
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
@@ -43,6 +42,7 @@ exports.post = [
         title: req.body.title,
         message: req.body.message,
         author: req.user.id,
+        time: DateTime.now(),
       });
 
       await post.save();
@@ -50,3 +50,22 @@ exports.post = [
     }
   }),
 ];
+
+exports.post_delete_confirm = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.postID).populate("author").exec();
+
+  res.render("confirm", {
+    user: res.locals.currentUser,
+    title: post.title,
+    message: post.message,
+    author: post.author.username,
+    postID: post.id,
+  });
+});
+
+exports.post_delete = asyncHandler(async (req, res, next) => {
+  if (res.locals.currentUser.isAdmin) {
+    await Post.findByIdAndDelete(req.params.postID).exec();
+  }
+  res.redirect("/");
+});
