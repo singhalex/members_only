1f6b302793d07004706617b943b36e2dbb7f9e6a
singhalex <singhkalex@gmail.com>
2024-06-18 22:32:58 -0700

    Set rate limit

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const passport = require("passport");
 const compression = require("compression");
 const helmet = require("helmet");
 
+//Set up rate limiter: maximum of 30 requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30,
+});
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
@@ -32,6 +39,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(limiter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
