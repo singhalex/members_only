619c0ce7b41b86dc88e8a47784ee0af8346a838c
singhalex <singhkalex@gmail.com>
2024-06-18 22:06:44 -0700

    Add time and date to post schema

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   title: { type: String, required: true, minLength: 3, maxLength: 50 },
-  message: { type: String, required: true, minLength: 3, maxLength: 200 },
+  message: { type: String, required: true, minLength: 3, maxLength: 400 },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  time: { type: Date, required: true },
+});
+
+PostSchema.virtual("time_date").get(function () {
+  return DateTime.fromJSDate(this.time).toLocaleString(DateTime.DATETIME_FULL);
 });
 
 module.exports = mongoose.model("Posts", PostSchema);
